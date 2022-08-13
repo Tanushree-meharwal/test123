f939ad6c95d6d544e12cdc8f67c5ebc1e776f86d
meharwaltanu14 <111167153+meharwaltanu14@users.noreply.github.com>
2022-08-12 22:33:55 -0700

    Add files via upload

---
+++ _warm up python -assignment 1.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Q-1 Write a command to get the Python version you are using.
+""" Ans-->command to get the python version- py"""
+
+
+# In[ ]:
+
+
+# Q-2 Write a python script to print Hello Python on the screen directly in the command line.
+print("Hello Python")
+
+
+# In[ ]:
+
+
+# Q-5 Write a python script to print Hello on the first line and Python on the second line.
+print("Hello \nPython")
+
+
+# In[ ]:
+
+
+# Q-6 Write a python script to print “MySirG” on the screen.
+print("MySirG")
+
+
+# In[ ]:
+
+
+# Q-7 Write a python script to print “Teacher’s Day” on the screen.
+print("Teacher’s Day")
+
+
+# In[ ]:
+
+
+# Q-8 Write a python script to print the value of a variable. Variable contains your name as data.
+a = "tanu"
+print(a)
+
+
+# In[ ]:
+
+
+# Q-9 Write a python script to create variables to store your name,age,qualification,years of experience and print it.
+Name = input("Enter your Name: ")
+Age = int(input("Enter your Age: "))
+Qualification = input("Enter your Qualification: ")
+YOE = (input("Enter your Experience:"))
+print(Name)
+print(Age)
+print(Qualification)
+print(YOE)
+
+
+# In[24]:
+
+
+# Q-10 Write a python script to print values of three variables, each in a new line. Variable contains integer values
+a = 10 
+b = 13
+c = 23
+print(str(a) + '\n' + str(b) + '\n'+ str(c))
+
+
+# In[ ]:
+
+
+
+
